004063e129af54986946b49afdf0cbe2025d4880
paulo0212 <rikuo.iwasaki@gmail.com>
2024-06-03 15:21:59 +0900

    FizzBuzzプログラムを作成

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+let message = "";
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    message += "Fizz";
+  }
+  if (i % 5 === 0) {
+    message += "Buzz";
+  }
+  message ||= i;
+  console.log(message);
+  message = "";
+}
