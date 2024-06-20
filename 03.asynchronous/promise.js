0d35d039423c6c01a68ec2d361d4a1e827e975f0
paulo0212 <rikuo.iwasaki@gmail.com>
2024-06-20 18:27:01 +0900

    Promise化したプログラムを実装

---
+++ 03.asynchronous/promise.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+import { runPromise, getPromise } from "./promisifiedFunctions.js";
+
+const db = new sqlite3.Database(":memory:");
+
+console.log("\n= = = = = ここからエラーなし = = = = =");
+runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    console.log("\n2. レコード追加");
+    return runPromise(db, "INSERT INTO books (title) VALUES (?)", "LoremIpsum");
+  })
+  .then((resolve) => {
+    console.log(`lastID : ${resolve.lastID}`);
+    console.log("\n3. レコード取得");
+    return getPromise(db, "SELECT * FROM books");
+  })
+  .then((book) => console.log(book))
+  .then(() => runPromise(db, "DROP TABLE books"));
+
+await timers.setTimeout(100);
+
+console.log("\n= = = = = ここからエラーあり = = = = =");
+runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    console.log("\n2. レコード追加");
+    return runPromise(db, "INSERT INTO books (title) VALUES (?)", null);
+  })
+  .catch((error) => console.error(error.message))
+  .then(() => {
+    console.log("\n3. レコード取得");
+    return getPromise(db, "SELECT author FROM books");
+  })
+  .catch((error) => console.error(error.message))
+  .then(() => runPromise(db, "DROP TABLE books"));
+
+await timers.setTimeout(100);
+
+db.close();
---
+++ 03.asynchronous/promisifiedFunctions.js
@@ -0,0 +1,23 @@
+export const runPromise = (db, query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+};
+
+export const getPromise = (db, query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.get(query, params, function (error, result) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
