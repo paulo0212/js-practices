54c32e7a4b6cd5ba48034c126f2491f4ceb1eefb
paulo0212 <rikuo.iwasaki@gmail.com>
2024-06-20 18:54:41 +0900

    async, awaitを使用したプログラムを実装

---
+++ 03.asynchronous/async.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import { runPromise, getPromise } from "./promisifiedFunctions.js";
+
+const db = new sqlite3.Database(":memory:");
+
+async function withoutError() {
+  await runPromise(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  console.log("\n2. レコード追加");
+  let resolve = await runPromise(
+    db,
+    "INSERT INTO books (title) VALUES (?)",
+    "LoremIpsum",
+  );
+  console.log(`lastID : ${resolve.lastID}`);
+
+  console.log("\n3. レコード取得");
+  let book = await getPromise(db, "SELECT * FROM books");
+  console.log(book);
+
+  await runPromise(db, "DROP TABLE books");
+}
+
+async function withError() {
+  await runPromise(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  console.log("\n2. レコード追加");
+  try {
+    let resolve = await runPromise(
+      db,
+      "INSERT INTO books (title) VALUES (?)",
+      null,
+    );
+    console.log(`lastID : ${resolve.lastID}`);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  console.log("\n3. レコード取得");
+  try {
+    let book = await getPromise(db, "SELECT author FROM books");
+    console.log(book);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  await runPromise(db, "DROP TABLE books");
+}
+
+console.log("\n= = = = = ここからエラーなし = = = = =");
+await withoutError();
+
+console.log("\n= = = = = ここからエラーあり = = = = =");
+await withError();
+
+await db.close();
