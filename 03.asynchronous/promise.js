cf9670f60ac0ead60c476a0939e322e455b4d304
paulo0212 <rikuo.iwasaki@gmail.com>
2024-06-24 15:25:12 +0900

    例外をcatchで処理するように変更

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -40,32 +40,30 @@ runPromise(
     console.log("\n2. レコード追加");
     return runPromise(db, "INSERT INTO books (title) VALUES (?)", null);
   })
-  .then(
-    (result) => {
-      console.log(`lastID : ${result.lastID}`);
-      console.log("\n3. レコード取得");
-      return getPromise(db, "SELECT author FROM books");
-    },
-    (error) => {
-      if (error.code === "SQLITE_CONSTRAINT") {
-        console.error(error.message);
-      } else {
-        throw error;
-      }
-      console.log("\n3. レコード取得");
-      return getPromise(db, "SELECT author FROM books");
-    },
-  )
-  .then(
-    (book) => {
-      console.log(book);
-      return runPromise(db, "DROP TABLE books");
-    },
-    (error) => {
-      if (error.code === "SQLITE_ERROR") {
-        console.error(error.message);
-      } else {
-        throw error;
-      }
-      return runPromise(db, "DROP TABLE books");
+  .then((result) => {
+    console.log(`lastID : ${result.lastID}`);
+    console.log("\n3. レコード取得");
+    return getPromise(db, "SELECT author FROM books");
+  })
+  .catch((error) => {
+    if (error.code === "SQLITE_CONSTRAINT") {
+      console.error(error.message);
+    } else {
+      throw error;
+    }
+    console.log("\n3. レコード取得");
+    return getPromise(db, "SELECT author FROM books");
+  })
+  .then((book) => {
+    console.log(book);
+    return runPromise(db, "DROP TABLE books");
+  })
+  .catch((error) => {
+    if (error.code === "SQLITE_ERROR") {
+      console.error(error.message);
+    } else {
+      throw error;
+    }
+    return runPromise(db, "DROP TABLE books");
+  })
   .then(() => closePromise(db));
